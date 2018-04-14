2f97ce18afddf5a11a300a389901daf65088f7b1
Fabio Sussarellu <sussarellu98@gmail.com>
2018-04-14 11:58:47 +0200

    wrritten beer model

--- apps/beers/models.py
+++ apps/beers/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Beer(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False)
+    description = models.TextField(max_length=1000)
+    picture = models.ImageField(upload_to="beersImages")
+    website = models.URLField()
