85302b9fd2570bd42e6f0da8abbd03d3f63b6f12
Fabio Sussarellu <sussarellu98@gmail.com>
2018-04-14 14:16:39 +0200

    added base template

--- clubinferno/settings/base.py
+++ clubinferno/settings/base.py
@@ -97,4 +97,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 STATIC_URL = '/static/'
