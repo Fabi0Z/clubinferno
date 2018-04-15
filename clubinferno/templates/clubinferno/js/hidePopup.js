5e25054558d7f851ed00205d7ee8ca837ff76632
Fabio Sussarellu <sussarellu98@gmail.com>
2018-04-15 12:53:10 +0200

    moved files

--- clubinferno/templates/clubinferno/js/hidePopup.js
+++
@@ -1,8 +0,0 @@
-function hidePopup() {
-    // When the user press the esc key, close the modal
-    document.addEventListener('keydown', function (event) {
-        if (event.keyCode === 27) {
-            location.href = '#';
-        }
-    });
-}
