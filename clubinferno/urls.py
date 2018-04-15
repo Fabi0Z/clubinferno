2a2537e2a453b2f043c451df453d533434854b2f
Fabio Sussarellu <sussarellu98@gmail.com>
2018-04-15 16:42:47 +0200

    enabled sponsor app

--- clubinferno/urls.py
+++ clubinferno/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('beers/', include('apps.beers.urls')),
+    path('sponsors/', include('apps.sponsors.urls')),
     path('admin/', admin.site.urls),
 ] + (static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +
      static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
