2ba0bc723c1139f900cd9e7e6f38d7126ca6d1ee
Fabio Sussarellu <sussarellu98@gmail.com>
2018-04-21 20:07:35 +0200

    added all needed for django jet

--- clubinferno/urls.py
+++ clubinferno/urls.py
@@ -21,6 +21,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('beers/', include('apps.beers.urls')),
     path('sponsors/', include('apps.sponsors.urls')),
+    path('jet/', include('jet.urls', 'jet')),
+    path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
     path('admin/', admin.site.urls),
 ] + (static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +
      static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
