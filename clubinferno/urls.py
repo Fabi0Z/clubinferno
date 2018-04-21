0a6baeaa4ee17212a0bd88d950b54b265589b6fd
Fabio Sussarellu <sussarellu98@gmail.com>
2018-04-21 20:21:06 +0200

    mapped home url

--- clubinferno/urls.py
+++ clubinferno/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='clubinferno/base.html')),
     path('beers/', include('apps.beers.urls')),
     path('sponsors/', include('apps.sponsors.urls')),
     path('jet/', include('jet.urls', 'jet')),
