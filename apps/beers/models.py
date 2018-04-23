7e03381d41269e0d2513f18d08cf9f7a92fb7d14
Fabio Sussarellu <sussarellu98@gmail.com>
2018-04-23 11:54:56 +0200

    added default beer image

--- apps/beers/models.py
+++ apps/beers/models.py
@@ -13,7 +13,8 @@ class BeerGenre(models.Model):
 class Beer(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=1000, blank=True, null=True)
-    picture = models.ImageField(upload_to="beersImages", blank=True, null=True)
+    picture = models.ImageField(
+        upload_to="beersImages", default="beersImages/default.png")
     website = models.URLField(blank=True, null=True)
     genre = models.ForeignKey(BeerGenre, on_delete=models.DO_NOTHING)
     country = CountryField(blank_label='(select country)')
