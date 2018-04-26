484e611e6440e400a3230855fc992ec70ba66dbf
Fabio Sussarellu <sussarellu98@gmail.com>
2018-04-26 18:14:43 +0200

    added startup script to project

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Upgrading pip"
+pip install --upgrade pip
+
+echo "Installing requirements"
+pip install -r /webapp/requirements/dev.txt
+
+echo "Waiting 30 second to wait the DB startup"
+sleep 30
+
+echo "Applying migrations"
+python /webapp/manage.py migrate
+
+echo "Running django server"
+python /webapp/manage.py runserver 0.0.0.0:8000
