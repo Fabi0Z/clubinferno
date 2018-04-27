a9a466bcae7f21213abfc6cc202393691df6bddf
Fabio Sussarellu <sussarellu98@gmail.com>
2018-04-27 17:22:29 +0200

    updated startup script to create a default superuser

--- start.sh
+++ start.sh
@@ -11,5 +11,8 @@ sleep 30
 echo "Applying migrations"
 python /webapp/manage.py migrate
 
+echo "Creating superuser"
+echo "from django.contrib.auth.models import User; User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser('admin', 'admin@example.com', 'clubinferno')" | python manage.py shell
+
 echo "Running django server"
 python /webapp/manage.py runserver 0.0.0.0:8000
