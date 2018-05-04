f20a5c8c6d031ebd856620ffc33123032480a12a
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2018-05-04 15:16:54 +0200

    changed waiting time

--- start.sh
+++ start.sh
@@ -5,14 +5,14 @@ pip install --upgrade pip
 echo "Installing requirements"
 pip install -r /webapp/requirements/dev.txt
 
-echo "Waiting 30 second to wait the DB startup"
-sleep 30
+echo "Waiting 10 second to wait the DB startup"
+sleep 10
 
 echo "Applying migrations"
 python /webapp/manage.py migrate
 
 echo "Creating superuser"
-echo "from django.contrib.auth.models import User; User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser('admin', 'admin@example.com', 'clubinferno')" | python manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser('admin', 'admin@example.com', 'clubinferno')" | python /webapp/manage.py shell
 
 echo "Running django server"
 python /webapp/manage.py runserver 0.0.0.0:8000
